1c188df0abd36ed8e69764b612a22d64f3fc4feb
Jesús Guerrero <jgs982@gmail.com>
2022-07-07 07:46:24 +0200

    first commit

---
+++ hooks/useQuiosco.js
@@ -0,0 +1,13 @@
+import { useContext } from "react"
+import QuioscoContext from "../context/QuioscoProvider"
+
+
+
+const useQuiosco = () => {
+
+    return(
+        useContext(QuioscoContext)
+    )
+}
+
+export default useQuiosco
---
+++ pages/resumen.js
@@ -0,0 +1,42 @@
+import Layout from '../layout/Layout'
+import useQuiosco from '../hooks/useQuiosco'
+import ResumenProducto from '../components/ResumenProducto'
+
+
+const Resumen = () => {
+
+    const { pedido } = useQuiosco()
+
+    return (
+        <Layout pagina='Resumen'>
+            <h1 className='text-4xl font-black'>
+                Resumen
+            </h1>
+
+            <p className='text-2xl my-10'>
+                Revisa tu Pedido
+            </p>
+
+            {
+                pedido.length===0
+                ? (
+                    <p className='text-center text-2xl'>
+                        No hay elementos en tu pedido...
+                    </p>
+                )
+                : (
+                    pedido.map(producto => {
+                        return(
+                            <ResumenProducto
+                                key={producto.id}
+                                producto={producto}
+                            />
+                        )
+                    })
+                )
+            }
+        </Layout>
+    )
+}
+
+export default Resumen
